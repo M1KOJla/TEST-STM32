55c74943165ba3a050c12a6e1a46857606947b19
Mihail Kolosov <m1kojlakolosov@gmail.com>
2025-10-08 18:18:06 +0300

    Переименование функций
      LED_PORT_init();
      LED_TIM_init ();

--- Inc/led_init.h
+++ Inc/led_init.h
@@ -4,7 +4,7 @@
 #include "stm32f1xx.h"
 #define SYSCLK_FREQ  8000000U							//макрос системной частоты МК
 
-void PORTB_init (void);							        //прототип функции инициализации PORTB PIN2&PIN5
-void TIM3_init (void);
+void LED_PORT_init (void);							//прототип функции инициализации PORTB PIN2&PIN5
+void LED_TIM_init (void);                                                       //прототип функции инициализации TIM3
 
 #endif 
