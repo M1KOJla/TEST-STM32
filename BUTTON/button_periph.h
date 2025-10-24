39939fe86c5669fdd223fada70489eaff99c2a64
Mihail Kolosov <m1kojlakolosov@gmail.com>
2025-10-24 15:48:35 +0300

    перенос TIM2_IRQHandler
    в button_periph.c должна быть ф-ия TIM2_IRQHandler, и из нее вызываться button_irq, которая в файле button_periph.h объевлена с extern.

--- BUTTON/button_periph.h
+++ BUTTON/button_periph.h
@@ -7,5 +7,6 @@
 void PORTA_init (void);								//прототип функции инициализации порта PORTA
 void TIM2_init (void);							        //прототип функции инициализации TIM2
 uint8_t read_button_state (void);                                               //прототип функции read_button_state
+extern void button_press_tracking_irq (void);
 
 #endif 
--- BUTTON/button_proc.h
+++ BUTTON/button_proc.h
@@ -3,7 +3,7 @@
 #include "stm32f1xx.h"
 
 void button_init(void);
-void button_press_tracking (void);
+void button_press_tracking_irq (void);
 uint8_t track_button_press_again (void);
 
 #endif
